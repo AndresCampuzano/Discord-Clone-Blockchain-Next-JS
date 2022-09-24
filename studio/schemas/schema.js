c3063982dde154d6ca9720bad7852d65f0ff36db
Andres Campuzano Garzon <hello@andrescampuzano.com>
2022-09-24 14:36:45 -0500

    add userSchema and conversationsSchema to sanity

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,7 +1,11 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+// Schemas
+import {userSchema} from "./userSchema";
+import {conversationsSchema} from "./conversationsSchema";
+
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([userSchema, conversationsSchema]),
 })
